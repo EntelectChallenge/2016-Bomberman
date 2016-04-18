48fe23ec3bf4fd01976e55fd5f2f4bc65f9e61ed
tjorriemorrie <jacoj82@gmail.com>
2016-04-18 18:09:37 +0200

    Changes to bot sample file

--- Sample Bots/Python3/bot.py
+++ Sample Bots/Python3/bot.py
@@ -1,38 +1,47 @@
+import argparse
+import json
+import logging
+import os
 import random
-import sys
-from os import path
 
-def main(output_path, player_key):
-    possible_moves = [
-        {'Command': 'DoNothing', 'CommandCode': -1},
-        {'Command': 'MoveUp', 'CommandCode': 1},
-        {'Command': 'MoveLeft', 'CommandCode': 2},
-        {'Command': 'MoveRight', 'CommandCode': 3},
-        {'Command': 'MoveDown', 'CommandCode': 4},
-        {'Command': 'PlaceBomb', 'CommandCode': 5},
-        {'Command': 'TriggerBomb', 'CommandCode': 6}]
-    move = random.choice(possible_moves)
-    move_file = open(path.join(output_path, 'move.txt'), 'w')
-    move_file.write(str(move['CommandCode']) + '\r\n')
-    move_file.close()
+logger = logging.getLogger()
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)-7s - [%(funcName)s] %(message)s')
+# uncomment for submission
+# logger.disabled = True
 
-    state_file = open(path.join(output_path, 'state.json'), 'r')
-    state_file.close()
+ACTIONS = {
+	-1: 'DoNothing',
+	1: 'MoveUp',
+	2: 'MoveLeft',
+	3: 'MoveRight',
+	4: 'MoveDown',
+	5: 'PlaceBomb',
+	6: 'TriggerBomb',
+}
+
+
+def main(player_key, output_path):
+
+	logger.info('Player key: {}'.format(player_key))
+	logger.info('Output path: {}'.format(output_path))
+
+	with open(os.path.join(output_path, 'state.json'), 'r') as f:
+		state = json.load(f)
+		logger.info('State: {}'.format(state))
+
+	action = random.choice(list(ACTIONS.keys()))
+	logger.info('Action: {}'.format(ACTIONS[action]))
+
+	with open(os.path.join(output_path, 'move.txt'), 'w') as f:
+		f.write('{}\n'.format(action))
+
 
 if __name__ == '__main__':
-    if(len(sys.argv) < 2):
-        player_key = ''
-    else:
-        player_key = sys.argv[1]
+	parser = argparse.ArgumentParser()
+	parser.add_argument('player_key', nargs='?')
+	parser.add_argument('output_path', nargs='?', default=os.getcwd())
+	args = parser.parse_args()
 
-    if(len(sys.argv) < 3):
-        output_path = ''
-    else:
-        output_path = sys.argv[2]
+	assert(os.path.isdir(args.output_path))
 
-    if (output_path != '' and path.exists(output_path) == False):
-       print
-       print ('Error: Output folder "' + sys.argv[1] + '" does not exist.')
-       exit(-1)
-
-    main(output_path, player_key)
+	main(args.player_key, args.output_path)
