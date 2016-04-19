ebf7579bf1cc228d12d5310b6cb34987c8188477
Hennie Brink <hennie.brink@entelect.co.za>
2016-04-19 17:37:18 +0200

    Fixed java sample bot not reading the state file

--- Sample Bots/Java/src/main/java/Main.java
+++ Sample Bots/Java/src/main/java/Main.java
@@ -1,6 +1,11 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -16,8 +21,18 @@ public class Main {
         System.out.println("Bot Key: " + botKey);
         System.out.println("Bot Dir: " + botDir);
 
+        Path path = Paths.get(botDir + File.separator + "state.json");
+
         File move = new File(botDir + File.separator + "move.txt");
         try {
+            List<String> lines = Files.readAllLines(path, Charset.defaultCharset());
+
+            StringBuilder fileContent = new StringBuilder();
+            for (String line : lines) {
+
+                fileContent.append(line);
+            }
+
             if(move.createNewFile()){
 
                 FileWriter write = new FileWriter(move);
